3ba113696e0b74690cda2b4e1e9a8ba525544cf0
MartryatusSofia <martryatuss@gmail.com>
2026-05-17 18:52:50 +0700

    Test 1

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,15 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Nilai sekarang:" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Setelah increment::" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Setelah decrement" + counter.getCount());
 		
 	}
 
